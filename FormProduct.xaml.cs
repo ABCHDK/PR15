5aa0b68bfbb3332ae174859183a4e915d706d9b8
ABCHDK <118878901+ABCHDK@users.noreply.github.com>
2024-12-07 18:00:44 +0300

    Add files via upload

---
+++ FormProduct.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Data;
+using System.Windows.Markup;
+
+namespace Torg_texnic
+{
+    /// <summary>
+    /// Логика взаимодействия для FormProduct.xaml
+    /// </summary>
+    public partial class FormProduct : Window
+    {
+        private readonly string connectionString = "Data Source=F:\\уп0301\\Torg_texnic.db;Version=3;"; 
+
+        public FormProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+                    SQLiteCommand command = new SQLiteCommand("SELECT * FROM Product", connection);
+                    SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    UsersDataGrid.ItemsSource = dataTable.DefaultView; // Привязка данных к DataGrid
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при загрузке данных: " + ex.Message);
+            }
+        }
+
+        
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Azure.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Torg_texnic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+
+            // Проверка имени пользователя и пароля
+            if (username == "admin" && password == "admin") // Пример проверки
+            {
+                MessageBox.Show("Вход выполнен успешно!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                // Переход на другую форму
+                FormProduct mainPage = new FormProduct();
+                mainPage.Show(); // Открываем новую форму
+                this.Close(); // Закрываем текущее окно
+            }
+            else
+            {
+                MessageBox.Show("Неверное имя пользователя или пароль.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
